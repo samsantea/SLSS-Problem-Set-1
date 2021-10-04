9c944864ef32b0e7384aff1cad88fca85ba5f0fc
Sam <sam.jver@gmail.com>
2021-10-04 12:31:28 -0700

    Added an option when responding to whether the user is ready for the quiz or not. If the answer is neither y or n, let the user know the bot took his/her answer as a no.

--- javier_samantha_p1q1.py
+++ javier_samantha_p1q1.py
@@ -12,25 +12,28 @@ import time
 quiz_score = 0
 
 # Greet the user and get the user's name.
-name = input(f"Good day! I'm the BTS quiz bot. What's your name?\n")
+username = input(f"Good day! I'm the BTS quiz bot. What's your name?\n")
 
 # Introduce the quiz.
-print(f"Hello, {name}! I'm going to ask you a few questions about BTS today.\n")
+print(f"Hello, {username}! I'm going to ask you a few questions about BTS today.\n")
 
-# Ask if the user is ready to start the quiz.
-start_quiz = input("Are you ready for my quiz?\nY/N\n")
+# Ask if the user is ready to start the quiz..
+start_quiz_answer = input("Are you ready for my quiz?\nY/N\n")
 
-if start_quiz.lower() in ["y", "yes"]:
-    # If the user says he/she is ready, run the quiz
+if start_quiz_answer.lower().strip(".?/!") in ["y", "yes"]:
+    # If the user says he/she is ready, run the quiz.
     run_quiz = True
-else:
-    # If the user is not ready, do not run the quiz
+elif start_quiz_answer.lower().strip(".?/!") in ["n", "no"]:
     run_quiz = False
-    print("Okay, see you later!")
+else:
+    # If the user is not ready, do not run the quiz.
+    run_quiz = False
+    print("Sorry, I don't get what you're saying. I'll take that as a no!")
 
-# If the run quiz variable is True, the quiz will begin.
+# If the run quiz variable is True, the quiz will begin..
 while run_quiz:
-    # List of questions and answers
+    # List with nested lists of questions and answers.
+    # In each nested list, the first element is the question and the second is the answer.
     questions = [
         ["What month is the anniversary of BTS? ", "june"],
         ["What is the first name of the youngest member in BTS?", "jungkook"],
@@ -43,38 +46,45 @@ while run_quiz:
         ["Finally, what is the English translation of BTS' full name?", "bulletproof boy scouts"]
     ]
 
-    # For each question, ask the question, get the user's answer, and check whether it is correct
+    # For each question, ask the question, get the user's answer, and check whether it is correct.
     for question in questions:
         print(question[0])
 
         user_answer = input()
 
-        # If it is correct, let the user know he/she was correct and add one to the quiz score
+        # If it is correct, let the user know he/she was correct and add one to the quiz score.
         if user_answer.lower().strip(".?/!") == question[1]:
             print("Correct!")
             quiz_score += 1
 
-        # Else, let the user know he/she was wrong
+        # Else, let the user know he/she was wrong.
         else:
             print("Incorrect.")
 
-    # Thank the user for taking the quiz and share the score.
+    # Thank the user for taking the quiz.
     print("Thanks for taking my quiz! Your score is...")
 
     time.sleep(5)
+
+    # Share the quiz score.
     print(f"{str(quiz_score)}/5 ({quiz_score / 5 * 100}%).")
 
-    # React differently based on the user's score.
+    # React differently based on the user's final score.
     if quiz_score > 4:
         print("Wow! You're a BTS genius!")
+
     elif quiz_score > 3:
         print("Great work!")
+
     elif quiz_score > 2:
         print("Good job.")
+
     elif quiz_score > 1:
         print("Don't worry, it happens to all of us.")
+
     elif quiz_score > 0:
         print("At least you tried...")
+
     else:
         print("Do you even know who BTS is?")
 
